4c87e9f52cfcbdf5abd2e5ef047451df7c635800
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2018-02-22 09:58:22 +0300

    Task 1: intro

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,11 +1,14 @@
 package ru.track;
 
 
+import java.util.Arrays;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class App2 {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
         String str = StringUtils.capitalize(args[0]);
         System.out.println(str);
     }
